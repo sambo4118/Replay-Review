880a812ea427eaa7f6d7d0e3ac3b06ee95df6434
sambo4118 <41778730+sambo4118@users.noreply.github.com>
2026-05-01 18:30:47 +0000

    clean slate

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.use(express.static('public'));
 app.use('/vendor', express.static('node_modules'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/front.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
